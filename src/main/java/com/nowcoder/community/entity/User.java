1fd949382080a7a8abe127843a12f0dba8798fba
Shawn <shawntimmel@gmail.com>
2021-07-27 16:34:29 +0800

    mybatis application.properties

---
+++ src/main/java/com/nowcoder/community/entity/User.java
@@ -0,0 +1,37 @@
+package com.nowcoder.community.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class User {
+    private int id;
+    private String username;
+    private String passport;
+    private String salt;
+    private String email;
+    private int type;
+    private int status; // 状态
+    private String activationCode; // 激活码
+    private String headerUrl; // 头像路径
+    private Date createTime;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", passport='" + passport + '\'' +
+                ", salt='" + salt + '\'' +
+                ", email='" + email + '\'' +
+                ", type=" + type +
+                ", status=" + status +
+                ", activationCode='" + activationCode + '\'' +
+                ", headerUrl='" + headerUrl + '\'' +
+                ", createTime=" + createTime +
+                '}';
+    }
+}
