11c5e5810671232267a9c0f5fbf346a0d74821a9
Shawn <shawntimmel@gmail.com>
2021-08-13 23:06:36 +0800

    MailClient

---
+++ src/main/java/com/nowcoder/community/util/MailClient.java
@@ -0,0 +1,22 @@
+package com.nowcoder.community.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class MailClient {
+    public static final Logger logger = LoggerFactory.getLogger(MailClient.class);
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String from;
+
+
+}
