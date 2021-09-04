c9d619c418fd7524a7cc7307bbee1ea3f35f37d7
Shawn <shawntimmel@gmail.com>
2021-09-05 02:03:31 +0800

    html resources

--- src/main/java/com/nowcoder/community/util/MailClient.java
+++ src/main/java/com/nowcoder/community/util/MailClient.java
@@ -5,7 +5,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 
 @Component
@@ -18,5 +22,19 @@ public class MailClient {
     @Value("${spring.mail.username}")
     private String from;
 
+    public void sendMail(String to, String subject, String content) {
+        try {
+            MimeMessage massage = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(massage);
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(content, true); // support html
+            mailSender.send(helper.getMimeMessage());
+        } catch (MessagingException e) {
+            logger.error("send mail error: " + e.getMessage());
+        }
+
+    }
 
 }
