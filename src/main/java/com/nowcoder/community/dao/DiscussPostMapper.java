3917aa0401baa7a23b9b2bae74b8a4268b99538b
Shawn <shawntimmel@gmail.com>
2021-09-06 23:16:52 +0800

    DiscussPost

---
+++ src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
@@ -0,0 +1,17 @@
+package com.nowcoder.community.dao;
+
+import com.nowcoder.community.entity.DiscussPost;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface DiscussPostMapper {
+// offset: start index of each page, limit: max index for each page
+    List<DiscussPost> selectDiscussPosts(int userId, int offset, int limit);
+
+// @Param: 参数别名 如果方法只有一个参数并且在<if>里使用就必须加别名
+    int selectDiscussPostRows(@Param("userId") int userId);
+
+}
